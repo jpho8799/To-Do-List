c211c9af19f79122c25021d2435c6070b9303bbf
Josephine Pho <josephinepho@josephines-air.myfiosgateway.com>
2022-06-11 15:56:36 -0400

    set up webpack config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+
+import test from './images/test.png'
+import style from './styles/styles.css'
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    devServer: {
+        static: './dist',
+    },
+
+    output: {
+        filename: '[name][contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[hash][ext][query]',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            title: 'Pixel Cafe',
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
+    ]
+
+
+};
