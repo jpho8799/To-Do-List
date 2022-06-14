f82425837faf562bbd896ef8943a46fb93290598
Josephine Pho <josephinepho@josephines-air.myfiosgateway.com>
2022-06-14 14:42:36 -0400

    added sliding nav bar

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ const newprojectIcon = document.querySelector("#new-project");
 
 
 navbarIcon.addEventListener('click', ()=>{
-    const navBar = document.querySelector('#navbar');
-    navBar.classList.toggle('navbar---toggle');
+    const navBar = document.querySelector('nav');
+    
+    if(navBar.classList.contains('navbar---toggleLtoR')){
+        navBar.classList.remove('navbar---toggleLtoR');
+        navBar.classList.add('navbar---toggleRtoL');
+    }else{
+        navBar.classList.remove('navbar---toggleRtoL');
+        navBar.classList.add('navbar---toggleLtoR');
+    }
+
     
 })
 
 newprojectIcon.addEventListener('click', ()=>{
-    
+
 })
 
 
