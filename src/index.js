b6170c327ea970c9b1c6816f1742d07eef5e28dc
Josephine Pho <josephinepho@josephines-air.myfiosgateway.com>
2022-06-13 23:14:40 -0400

    changed page layout style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import './styles.css';
 
+const navbarIcon = document.querySelector("#navIcon");
+const newprojectIcon = document.querySelector("#new-project");
 
+
+navbarIcon.addEventListener('click', ()=>{
+    const navBar = document.querySelector('#navbar');
+    navBar.classList.toggle('navbar---toggle');
+    
+})
+
+newprojectIcon.addEventListener('click', ()=>{
+    
+})
 
 
