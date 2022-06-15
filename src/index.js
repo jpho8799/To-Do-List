0ae0b6f9450bf39fc83589bcd5c9e421a8fad37c
Josephine Pho <josephinepho@josephines-air.myfiosgateway.com>
2022-06-15 19:41:30 -0400

    added forms styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 
 const navbarIcon = document.querySelector("#navIcon");
 const newprojectIcon = document.querySelector("#new-project");
-
+const newtaskIcon = document.querySelector('#new-task');
 
 navbarIcon.addEventListener('click', ()=>{
     const navBar = document.querySelector('nav');
@@ -14,12 +14,22 @@ navbarIcon.addEventListener('click', ()=>{
         navBar.classList.remove('navbar---toggleRtoL');
         navBar.classList.add('navbar---toggleLtoR');
     }
-
-    
-})
+});
 
 newprojectIcon.addEventListener('click', ()=>{
+    const projectForm = document.getElementById('projectForm');
+    projectForm.style.display = 'flex';
 
+});
+
+newtaskIcon.addEventListener('click', ()=>{
+    const taskForm = document.getElementById('taskForm');
+    taskForm.style.display = 'flex';
 })
+
+
+
+
+
 
 
